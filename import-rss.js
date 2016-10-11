ed1bef9ea4e81f8d18baf6d2efc581f883a1e341
Lantrix <lantrix@pobox.com>
2016-10-12 00:35:39 +1100

    Importing to S3 bucket now to see initial data from archive

--- import-rss.js
+++ import-rss.js
@@ -1,18 +1,29 @@
 /*eslint-env node*/
 "use strict";
 
-var parser = require("rss-parser");
-var jsonfile = require("jsonfile");
+const parser = require("rss-parser");
+const aws = require("aws-sdk");
+const s3 = new aws.S3();
 
-var dataFile = "data/rssdata.json";
-var sourceRss = "http://microflapi.com/angryhuman/angryhuman-archive.xml";
+const s3BucketName = "angryhumanrss";
+const region = "us-east-1";
+const dataFile = "data/rssdata";
+var sourceRss = "data/angryhuman-archive.xml";
 
-parser.parseURL(sourceRss, function(err, parsed) {
+parser.parseFile(sourceRss, function(err, parsed) {
 	console.log("Feed Title " + parsed.feed.title);
 	parsed.feed.entries.forEach(function(entry) {
 		console.log(entry.title + ":" + entry.link);
 	});
-	jsonfile.writeFile(dataFile, parsed.feed.entries, function (err) {
-		console.error(err);
+	aws.config.region = region;
+	var params = {
+		Bucket: s3BucketName,
+		Key: dataFile,
+		Body: JSON.stringify(parsed.feed.entries),
+		ContentType: "application/json"
+	};
+	s3.putObject(params, function(err, data) {
+		if (err) console.log(err, err.stack); // an error occurred
+		else     console.log(data);           // successful response
 	});
 });
