b5185d4088c2e714c3ad38826bca1c1dcdbd3f5c
Lantrix <lantrix@pobox.com>
2016-10-16 22:40:43 +1100

    Updated tests for lambda

--- test/lambda-test.js
+++ test/lambda-test.js
@@ -1,30 +1,31 @@
 // Testing requirements
-var chai = require("chai");
-var jsonfile = require("jsonfile");
-var expect = chai.expect;
-var assert = chai.assert;
-
-// Code requirements
+const jsonfile = require("jsonfile");
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+const AWS = require("aws-sdk-mock");
 
 
-// in your testfile
+//App requirements
 var lib = require("../lib/rss.js");
-var lambda = require("../handler.js");
-// var sinon = require("sinon");
+var app = require("../handler.js");
 
-describe("Lambda Test", function() {
-	it("lambda handler runs", function(done) {
-		// sinon.stub(lambda, 'instantiateFeedObject', function() {
-		//   // whatever you would like innerLib.toCrazyCrap to do under test
-		// });
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+chai.should();
 
-		let event = jsonfile.readFile("../event.json");
-		let a = lambda.generaterss(event);
-		expect(a).to.be.a("string");
-		expect(a).to.equal("Published XML");
-		done();
-		// sinon.assert.calledOnce(lib.instantiateFeedObject); // sinon assertion
+describe("Generate RSS in S3", function () {
 
-		// lib.instantiateFeedObject.restore(); // restore original functionality
+	before(function () {
+		AWS.mock("S3", "getObject", function (params, callback) {
+			callback(null, "dummy-data");
+		});
+	});
+
+	it("Lambda handler runs", function() {
+		app.generaterss().should.eventually.equal("dummy-data");
+	});
+
+	after(function () {
+		AWS.restore("S3", "getObject");
 	});
 });
